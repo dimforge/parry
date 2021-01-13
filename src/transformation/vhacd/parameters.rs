dafa55066b5283db30bb0850e1916ae5e2a3f0fd
Crozet Sébastien <developer@crozet.re>
2021-01-13 16:34:49 +0100

    VHACD: remove unused parameters.

--- src/transformation/vhacd/parameters.rs
+++ src/transformation/vhacd/parameters.rs
@@ -6,19 +6,12 @@ pub struct VHACDParameters {
     pub concavity: Real,
     pub alpha: Real,
     pub beta: Real,
-    pub min_volume_per_ch: Real,
     pub resolution: u32,
-    pub max_num_vertices_per_ch: u32,
     pub plane_downsampling: u32,
     pub convex_hull_downsampling: u32,
-    pub pca: u32,
-    // 0: voxel-based (recommended), 1: tetrahedron-based.
-    pub mode: u32,
     pub fill_mode: FillMode,
     pub convex_hull_approximation: bool,
     pub max_convex_hulls: u32,
-    // This will project the output convex hull vertices onto the original source mesh to increase the floating point accuracy of the results.
-    pub project_hull_vertices: bool,
 }
 
 impl Default for VHACDParameters {
@@ -30,13 +23,8 @@ impl Default for VHACDParameters {
             convex_hull_downsampling: 4,
             alpha: 0.05,
             beta: 0.05,
-            pca: 0,
-            mode: 0,
-            max_num_vertices_per_ch: 64,
-            min_volume_per_ch: 0.0001,
             convex_hull_approximation: true,
             max_convex_hulls: 1024,
-            project_hull_vertices: true,
             fill_mode: FillMode::FloodFill,
         }
     }
