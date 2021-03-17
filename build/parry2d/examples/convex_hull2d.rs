1cc28820bebabc6817f92ba0618540e396a38095
Crozet Sébastien <developer@crozet.re>
2021-03-17 10:17:12 +0100

    Fix the compilation of examples.

--- build/parry2d/examples/convex_hull2d.rs
+++ build/parry2d/examples/convex_hull2d.rs
@@ -4,10 +4,18 @@ use na::Point2;
 use parry2d::transformation;
 
 fn main() {
-    let mut points = Vec::new();
-    for _ in 0usize..100000 {
-        points.push(rand::random::<Point2<f32>>() * 2.0);
-    }
+    let points = vec![
+        Point2::new(0.77705324, 0.05374551),
+        Point2::new(0.35096353, 0.9873069),
+        Point2::new(0.09537989, 0.44411153),
+        Point2::new(0.108208835, 0.72445065),
+        Point2::new(0.7661844, 0.86163324),
+        Point2::new(0.5185994, 0.66594696),
+        Point2::new(0.768981, 0.23657233),
+        Point2::new(0.058607936, 0.09037298),
+        Point2::new(0.8818559, 0.3804205),
+        Point2::new(0.9571466, 0.17664945),
+    ];
 
     let _ = transformation::convex_hull(&points[..]);
 }
