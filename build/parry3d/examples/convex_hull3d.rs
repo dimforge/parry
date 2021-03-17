290d0f59482c48c9e0304c2cc4eaf7f5b84794f9
Crozet Sébastien <developer@crozet.re>
2021-03-17 10:17:12 +0100

    Fix the compilation of examples.

--- build/parry3d/examples/convex_hull3d.rs
+++ build/parry3d/examples/convex_hull3d.rs
@@ -4,10 +4,18 @@ use na::Point3;
 use parry3d::transformation;
 
 fn main() {
-    let mut points = Vec::new();
-    for _ in 0usize..100000 {
-        points.push(rand::random::<Point3<f32>>() * 2.0);
-    }
+    let points = vec![
+        Point3::new(0.77705324, 0.05374551, 0.9822232),
+        Point3::new(0.35096353, 0.9873069, 0.28922123),
+        Point3::new(0.09537989, 0.44411153, 0.05486667),
+        Point3::new(0.108208835, 0.72445065, 0.6669141),
+        Point3::new(0.7661844, 0.86163324, 0.80507314),
+        Point3::new(0.5185994, 0.66594696, 0.072779536),
+        Point3::new(0.768981, 0.23657233, 0.44346774),
+        Point3::new(0.058607936, 0.09037298, 0.017009139),
+        Point3::new(0.8818559, 0.3804205, 0.25173646),
+        Point3::new(0.9571466, 0.17664945, 0.6029223),
+    ];
 
     let _ = transformation::convex_hull(&points[..]);
 }
