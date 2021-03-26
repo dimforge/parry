1ea116833b41d72993e436104092d1fa8c838e73
Crozet Sébastien <developer@crozet.re>
2021-03-26 18:11:32 +0100

    Refactor the ball/convex polyhedron closest points computation.

--- src/query/closest_points/closest_points_ball_convex_polyhedron.rs
+++ src/query/closest_points/closest_points_ball_convex_polyhedron.rs
@@ -1,8 +1,6 @@
-use crate::math::{Isometry, Point, Real};
+use crate::math::{Isometry, Real};
 use crate::query::ClosestPoints;
-use crate::shape::{Ball, FeatureId, Shape};
-
-use na::{self, Unit};
+use crate::shape::{Ball, Shape};
 
 /// ClosestPoints between a ball and a convex polyhedron.
 ///
@@ -15,7 +13,16 @@ pub fn closest_points_ball_convex_polyhedron(
     shape2: &(impl Shape + ?Sized),
     prediction: Real,
 ) -> ClosestPoints {
-    closest_points_convex_polyhedron_ball(&pos12.inverse(), shape2, ball1, prediction).flipped()
+    match crate::query::details::contact_ball_convex_polyhedron(pos12, ball1, shape2, prediction) {
+        Some(contact) => {
+            if contact.dist <= 0.0 {
+                ClosestPoints::Intersecting
+            } else {
+                ClosestPoints::WithinMargin(contact.point1, contact.point2)
+            }
+        }
+        None => ClosestPoints::Disjoint,
+    }
 }
 
 /// ClosestPoints between a convex polyhedron and a ball.
@@ -29,37 +36,14 @@ pub fn closest_points_convex_polyhedron_ball(
     ball2: &Ball,
     prediction: Real,
 ) -> ClosestPoints {
-    let center2_1 = Point::from(pos12.translation.vector);
-    let (proj, f1) = shape1.project_local_point_and_get_feature(&center2_1);
-
-    let dist;
-    let normal1;
-    if let Some((dir1, len)) =
-        Unit::try_new_and_get(proj.point - center2_1, crate::math::DEFAULT_EPSILON)
-    {
-        if proj.is_inside {
-            dist = -len - ball2.radius;
-            normal1 = dir1;
-        } else {
-            dist = len - ball2.radius;
-            normal1 = -dir1;
+    match crate::query::details::contact_convex_polyhedron_ball(pos12, shape1, ball2, prediction) {
+        Some(contact) => {
+            if contact.dist <= 0.0 {
+                ClosestPoints::Intersecting
+            } else {
+                ClosestPoints::WithinMargin(contact.point1, contact.point2)
+            }
         }
-    } else {
-        if f1 == FeatureId::Unknown {
-            // We cant do anything more at this point.
-            return None;
-        }
-
-        dist = -ball2.radius;
-        normal1 = shape1.feature_normal_at_point(f1, &proj.point)?;
+        None => ClosestPoints::Disjoint,
     }
-
-    if dist <= prediction {
-        let normal2 = pos12.inverse_transform_unit_vector(&-normal1);
-        let point2 = Point::from(*normal2 * ball2.radius);
-        let point1 = proj.point;
-        return Some(ClosestPoints::new(point1, point2, normal1, normal2, dist));
-    }
-
-    None
 }
