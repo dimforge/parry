5c2fb5a2bad2e584cc8b74bafb928cf0e71deffe
Sébastien Crozet <developer@crozet.re>
2021-10-14 11:41:38 +0200

    Add a basic OBB computation helper.

--- src/utils/cov.rs
+++ src/utils/cov.rs
@@ -2,6 +2,11 @@ use crate::math::{Matrix, Point, Real};
 
 /// Computes the covariance matrix of a set of points.
 pub fn cov(pts: &[Point<Real>]) -> Matrix<Real> {
+    center_cov(pts).1
+}
+
+/// Computes the center and the covariance matrix of a set of points.
+pub fn center_cov(pts: &[Point<Real>]) -> (Point<Real>, Matrix<Real>) {
     let center = crate::utils::center(pts);
     let mut cov: Matrix<Real> = na::zero();
     let normalizer: Real = 1.0 / (pts.len() as Real);
@@ -12,5 +17,5 @@ pub fn cov(pts: &[Point<Real>]) -> Matrix<Real> {
         cov += cp * (cp * normalizer).transpose();
     }
 
-    cov
+    (center, cov)
 }
---
+++ src/utils/obb.rs
@@ -0,0 +1,33 @@
+use crate::math::{Isometry, Point, Real, Rotation, Translation, Vector, DIM};
+use crate::shape::Cuboid;
+
+/// Computes an oriented bounding box for the given set of points.
+pub fn obb(pts: &[Point<Real>]) -> (Isometry<Real>, Cuboid) {
+    let cov = crate::utils::cov(pts);
+    let mut eigv = cov.symmetric_eigen().eigenvectors;
+
+    if eigv.determinant() < 0.0 {
+        eigv = -eigv;
+    }
+
+    let mut mins = Vector::repeat(Real::MAX);
+    let mut maxs = Vector::repeat(-Real::MAX);
+
+    for pt in pts {
+        for i in 0..DIM {
+            let dot = eigv.column(i).dot(&pt.coords);
+            mins[i] = mins[i].min(dot);
+            maxs[i] = maxs[i].max(dot);
+        }
+    }
+
+    #[cfg(feature = "dim2")]
+    let rot = Rotation::from_rotation_matrix(&na::Rotation2::from_matrix_unchecked(eigv));
+    #[cfg(feature = "dim3")]
+    let rot = Rotation::from_rotation_matrix(&na::Rotation3::from_matrix_unchecked(eigv));
+
+    (
+        rot * Translation::from((maxs + mins) / 2.0),
+        Cuboid::new((maxs - mins) / 2.0),
+    )
+}
