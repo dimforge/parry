31123c07bfabb2264ceb271c315fcd037a78ac45
Thierry Berger <contact@thierryberger.com>
2024-07-23 14:51:10 +0200

    explicit examples

--- publish.sh
+++ publish.sh
@@ -9,24 +9,32 @@ cp -r LICENSE README.md $tmp/.
 
 ### Publish the 2D version.
 sed 's#\.\./\.\./src#src#g' crates/parry2d/Cargo.toml > $tmp/Cargo.toml
+rm -rf $tmp/examples
+cp -r crates/parry2d/examples examples
 currdir=`pwd`
 cd $tmp && cargo publish
 cd $currdir
 
 ### Publish the 2D f64 version.
 sed 's#\.\./\.\./src#src#g' crates/parry2d-f64/Cargo.toml > $tmp/Cargo.toml
+rm -rf $tmp/examples
+cp -r crates/parry2d/examples examples
 cd $tmp && cargo publish
 cd $currdir
 
 
 ### Publish the 3D version.
 sed 's#\.\./\.\./src#src#g' crates/parry3d/Cargo.toml > $tmp/Cargo.toml
+rm -rf $tmp/examples
+cp -r crates/parry3d/examples $tmp/examples
 cp -r LICENSE README.md $tmp/.
 cd $tmp && cargo publish
 cd $currdir
 
 ### Publish the 3D f64 version.
 sed 's#\.\./\.\./src#src#g' crates/parry3d-f64/Cargo.toml > $tmp/Cargo.toml
+rm -rf $tmp/examples
+cp -r crates/parry3d/examples $tmp/examples
 cp -r LICENSE README.md $tmp/.
 cd $tmp && cargo publish
 
