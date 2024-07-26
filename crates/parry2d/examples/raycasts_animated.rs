1ae2768aa8c0ec65764facea8e17b24ce6852e0a
Thierry Berger <contact@thierryberger.com>
2024-07-26 15:49:54 +0200

    ximplified raycasts example

--- crates/parry2d/examples/raycasts_animated.rs
+++ crates/parry2d/examples/raycasts_animated.rs
@@ -1,56 +1,53 @@
 use macroquad::prelude::*;
 use nalgebra::{Isometry2, Point2, UnitComplex, Vector2};
+use parry2d::math::Isometry;
 use parry2d::query::{Ray, RayCast};
-use parry2d::shape::{Cuboid, TriMesh};
+use parry2d::shape::Cuboid;
 
 const RENDER_SCALE: f32 = 30.0;
 
 #[macroquad::main("parry2d::query::RayCast")]
 async fn main() {
-    let animation_scale = 2.0;
-    let animation_rotation = UnitComplex::new(0.008);
-    let spikes_render_pos = Point2::new(300.0, 300.0);
+    let animation_scale = 1.4;
+    let animation_rotation = 0.04;
+    let cube_render_pos = Point2::new(300.0, 300.0);
 
     for i in 1.. {
         clear_background(BLACK);
 
         /*
          *
-         * Compute the rotated/scaled cuboid.
+         * Compute the scaled cuboid.
          *
          */
-        let mut animated_cube = Cuboid::new(Vector2::new(2.0, 2.0)).to_polyline();
-        animated_cube.iter_mut().for_each(|pt| {
-            *pt = animation_rotation.powf(i as f32)
-                * *pt
-                * ((i as f32 / 100.0).sin().abs() * animation_scale);
-        });
-        let trimesh = TriMesh::from_polygon(animated_cube).unwrap();
+        let cube =
+            Cuboid::new(Vector2::new(2.0, 2.0) * ((i as f32 / 50.0).sin().abs() * animation_scale));
+        let rotation = Isometry2::rotation(0.008_f32 * i as f32);
         /*
          *
          * Prepare a Raycast and compute its result against the shape.
          *
          */
         let ray = Ray::new(
-            Point2::new(2.0, 1.0),
-            animation_rotation.powf(i as f32 * 2f32) * Vector2::y(),
+            Point2::new(2.0, 2.0),
+            UnitComplex::new(animation_rotation * i as f32) * -Vector2::x(),
         );
-        let toi = trimesh.cast_ray(&Isometry2::identity(), &ray, std::f32::MAX, true);
+        let toi = cube.cast_ray(&rotation, &ray, std::f32::MAX, true);
 
         /*
          *
-         * Render the raycast's result.
+         * Render the raycast's result.c
          *
          */
         if let Some(toi) = toi {
             if toi == 0f32 {
-                draw_point(ray.origin, RENDER_SCALE, spikes_render_pos, YELLOW);
+                draw_point(ray.origin, RENDER_SCALE, cube_render_pos, YELLOW);
             } else {
                 drawline_from_to(
                     ray.origin,
                     ray.origin + ray.dir * toi,
                     RENDER_SCALE,
-                    spikes_render_pos,
+                    cube_render_pos,
                     GREEN,
                 );
             }
@@ -59,7 +56,7 @@ async fn main() {
                 ray.origin,
                 ray.origin + ray.dir * 1000f32,
                 RENDER_SCALE,
-                spikes_render_pos,
+                cube_render_pos,
                 RED,
             );
         }
@@ -69,16 +66,28 @@ async fn main() {
          * Render the cuboid.
          *
          */
-        draw_polygon(&trimesh.vertices(), RENDER_SCALE, spikes_render_pos, GREEN);
+        draw_polygon(
+            &cube.to_polyline(),
+            &rotation,
+            RENDER_SCALE,
+            cube_render_pos,
+            GREEN,
+        );
 
         next_frame().await
     }
 }
 
-fn draw_polygon(polygon: &[Point2<f32>], scale: f32, shift: Point2<f32>, color: Color) {
+fn draw_polygon(
+    polygon: &[Point2<f32>],
+    rotation: &Isometry<f32>,
+    scale: f32,
+    shift: Point2<f32>,
+    color: Color,
+) {
     for i in 0..polygon.len() {
-        let a = polygon[i];
-        let b = polygon[(i + 1) % polygon.len()];
+        let a = rotation * polygon[i];
+        let b = rotation * polygon[(i + 1) % polygon.len()];
         draw_line(
             a.x * scale + shift.x,
             a.y * scale + shift.y,
