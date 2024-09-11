674a8f9592a6c166c9baf5a35d5999b499b3437b
Thierry Berger <contact@thierryberger.com>
2024-09-11 10:10:01 +0200

    non-crashing version

--- crates/parry3d/examples/convex_decomposition.rs
+++ crates/parry3d/examples/convex_decomposition.rs
@@ -4,8 +4,7 @@ extern crate nalgebra as na;
 
 use common_macroquad::mquad_mesh_from_points;
 use macroquad::prelude::*;
-use na::Vector3;
-use parry3d::shape::{Cuboid, SharedShape};
+use parry3d::{math::Point, shape::SharedShape};
 
 #[macroquad::main("parry2d::utils::point_in_poly2d")]
 async fn main() {
@@ -24,26 +23,61 @@ async fn main() {
         /*
          * Initialize the shapes.
          */
-        let cube1 = Cuboid::new(Vector3::repeat(0.5)).to_trimesh();
 
-        dbg!("before convex");
-        let convex_mesh = SharedShape::convex_decomposition(&cube1.0, &cube1.1);
-        dbg!("after convex, not reached");
-        let trimesh_convex = convex_mesh.0.as_trimesh().unwrap();
+        //// This cube1 ends up in infinite loop if attempting to call `convex_decomposition` on it
+        //
+        // let cube1 = Cuboid::new(Vector3::repeat(0.5)).to_trimesh();
+        // dbg!("before convex");
+        // let convex_mesh = SharedShape::convex_decomposition(&cube1.0, &cube1.1);
+        // dbg!("after convex, not reached");
+        // let trimesh_convex = convex_mesh.0.as_trimesh().unwrap();
+        //
+        ////
 
-        /*
-         * Display the shapes.
-         */
-        let trimesh_raw = (
-            trimesh_convex.vertices().to_vec(),
-            trimesh_convex.indices().to_vec(),
-        );
-        let mesh = mquad_mesh_from_points(
-            &trimesh_raw,
-            Vec3::new(1f32, 3f32, 3f32),
-            Color::from_rgba(200, 200, 200, 150),
-        );
-        draw_mesh(&mesh);
+        //
+        // Those values are from https://github.com/dimforge/rapier/issues/223#issuecomment-927816118
+
+        let points = [
+            Point::new(-0.74, -1.74, 52.3025),
+            Point::new(-0.74, -1.74, -52.3025),
+            Point::new(0.74, -1.74, -52.3025),
+            Point::new(0.74, -1.74, 52.3025),
+            Point::new(-0.74, 1.74, 52.3025),
+            Point::new(-0.74, 1.74, -52.3025),
+            Point::new(0.74, 1.74, -52.3025),
+            Point::new(0.74, 1.74, 52.3025),
+        ];
+        let indices = [
+            [4, 5, 0],
+            [5, 1, 0],
+            [5, 6, 1],
+            [6, 2, 1],
+            [6, 7, 3],
+            [2, 6, 3],
+            [7, 4, 0],
+            [3, 7, 0],
+            [0, 1, 2],
+            [3, 0, 2],
+            [7, 6, 5],
+            [4, 7, 5],
+        ];
+
+        let convex_mesh = SharedShape::convex_decomposition(&points, &indices);
+        let trimesh_convex_compound = convex_mesh.as_compound().unwrap();
+
+        for s in trimesh_convex_compound.shapes() {
+            let trimesh_convex = s.1.as_convex_polyhedron().unwrap().to_trimesh();
+
+            /*
+             * Display the shapes.
+             */
+            let mesh = mquad_mesh_from_points(
+                &trimesh_convex,
+                Vec3::new(1f32, 3f32, 3f32),
+                Color::from_rgba(200, 200, 200, 150),
+            );
+            draw_mesh(&mesh);
+        }
         next_frame().await
     }
 }
