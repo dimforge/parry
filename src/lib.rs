7494836542e906a247076c5fff32a50c78d4eea4
Thierry Berger <contact@thierryberger.com>
2024-10-01 09:44:54 +0200

    clean incorrect comments

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ the rust programming language.
 #![allow(clippy::type_complexity)] // Complains about closures that are fairly simple.
 #![doc(html_root_url = "http://docs.rs/parry/0.1.1")]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![deny(unused_qualifications)] // TODO: deny that everytime
+#![deny(unused_qualifications)]
 
 #[cfg(all(
     feature = "simd-is-enabled",
