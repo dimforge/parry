dc0f610e99525e681589b204b24239ad814d654e
Thierry Berger <contact@thierryberger.com>
2024-10-18 16:32:48 +0200

    fmt

--- crates/parry2d/examples/project_point2d.rs
+++ crates/parry2d/examples/project_point2d.rs
@@ -1,4 +1,6 @@
-use common_macroquad::dim2::{draw_line_2d, draw_trimesh2, lissajous_2d, mquad_from_na, na_from_mquad};
+use common_macroquad::dim2::{
+    draw_line_2d, draw_trimesh2, lissajous_2d, mquad_from_na, na_from_mquad,
+};
 use macroquad::prelude::*;
 use nalgebra::{Point3, UnitComplex, Vector2};
 use parry2d::math::{Isometry, Translation};
