a6f6503556ad3ea02cde8a6cf80db0f47afd4c00
Thierry Berger <contact@thierryberger.com>
2024-12-17 15:18:19 +0100

    better example with all cases shown at once

--- crates/parry2d/examples/debug_shape_cast.rs
+++ crates/parry2d/examples/debug_shape_cast.rs
@@ -2,32 +2,21 @@ mod common_macroquad2d;
 
 use common_macroquad2d::draw_point;
 use macroquad::prelude::*;
-use nalgebra::{Isometry2, Point2, UnitComplex, Vector2};
+use nalgebra::{Isometry2, Point2, Vector2};
 use parry2d::math::Isometry;
-use parry2d::query::{self, Ray, RayCast, ShapeCastOptions};
-use parry2d::shape::{Ball, ConvexPolygon, Cuboid, Shape, SharedShape};
+use parry2d::query::{self, Ray, ShapeCastOptions};
+use parry2d::shape::{Ball, ConvexPolygon, Shape};
 
 const RENDER_SCALE: f32 = 1.0;
 const BALLCAST_WIDTH: f32 = 16.0;
 
 #[macroquad::main("raycasts_animated")]
 async fn main() {
-    let animation_scale = 1.4;
-    let animation_rotation = 0.04;
-
-    for i in 1.. {
+    for _i in 1.. {
         clear_background(BLACK);
 
         let screen_shift =
-            Point2::new(screen_width() / 2.0, screen_height() / 2.0) + Vector2::new(-300.0, -200.0);
-        /*
-         *
-         * Compute the scaled cuboid.
-         *
-         */
-        /*let cube =
-            Cuboid::new(Vector2::new(2.0, 2.0) * ((i as f32 / 50.0).sin().abs() * animation_scale));
-        let cube_pose = Isometry2::rotation(0.008 * i as f32);*/
+            Point2::new(screen_width() / 2.0, screen_height() / 2.0) + Vector2::new(-500.0, -200.0);
 
         let to_cast_against = ConvexPolygon::from_convex_polyline(
             [
@@ -40,74 +29,37 @@ async fn main() {
         )
         .expect("Failed to create ConvexPolygon from polyline");
         let to_cast_against_pose = Isometry2::rotation(0f32);
-        let target_pos: Point2<_> = [631.4779, 250.20587].into();
+
         let mouse_pos = mouse_position();
         let mouse_position_world =
             (Point2::<f32>::new(mouse_pos.0, mouse_pos.1) - screen_shift.coords) / RENDER_SCALE;
-        let target_pos = mouse_position_world;
-        let pf_source_pos: Point2<f32> = [264.0, 440.0].into();
-        /*
-         *
-         * Prepare a Raycast and compute its result against the shape.
-         *
-         */
-        let ray = Ray::new(target_pos, pf_source_pos - target_pos);
-
-        //let toi = to_cast_against.cast_ray(&to_cast_against_pose, &ray, std::f32::MAX, true);
-
-        let pos1 = target_pos;
-        let vel1 = pf_source_pos - target_pos;
-        let g1 = Ball::new(BALLCAST_WIDTH);
-        let pos2 = [0.0, 0.0];
-        let vel2 = [0.0, 0.0];
-        let g2 = to_cast_against.clone_dyn();
-
-        let toi = query::cast_shapes(
-            &pos1.into(),
-            &vel1,
-            &g1,
-            &pos2.into(),
-            &vel2.into(),
-            &*g2,
-            ShapeCastOptions::with_max_time_of_impact(1.0),
-        )
-        .unwrap();
-
-        /*
-         *
-         * Render the raycast's result.
-         *
-         */
-        drawcircle_at(pos1, BALLCAST_WIDTH, RENDER_SCALE, screen_shift, ORANGE);
-        if let Some(toi) = toi {
-            if toi.time_of_impact == 0f32 {
-                draw_point(ray.origin, RENDER_SCALE, screen_shift, YELLOW);
-                dbg!("wath");
-            } else {
-                drawline_from_to(
-                    ray.origin,
-                    (ray.point_at(toi.time_of_impact).coords).into(),
-                    RENDER_SCALE,
-                    screen_shift,
-                    GREEN,
-                );
-            }
-            drawcircle_at(
-                (ray.point_at(toi.time_of_impact).coords).into(),
-                16.0,
-                RENDER_SCALE,
-                screen_shift,
-                GREEN,
-            );
-        } else {
-            drawline_from_to(
-                ray.origin,
-                ray.origin + ray.dir,
-                RENDER_SCALE,
-                screen_shift,
-                RED,
-            );
-        }
+        let target_pos: Point2<f32> = [264.0, 440.0].into();
+        shape_cast_debug(
+            screen_shift,
+            target_pos,
+            mouse_position_world,
+            to_cast_against.clone(),
+        );
+        // Those 2 fail with `min_bound >= _eps_tol`, fixed with a tolerance * 100
+        shape_cast_debug(
+            screen_shift,
+            [675.0, 255.0].into(),
+            target_pos,
+            to_cast_against.clone(),
+        );
+        shape_cast_debug(
+            screen_shift,
+            [674.0, 257.0].into(),
+            target_pos,
+            to_cast_against.clone(),
+        );
+        // This fails with `niter == 100` (and `niter == 100_000`), fixed with a tolerance * 10_000
+        shape_cast_debug(
+            screen_shift,
+            [623.0, 256.0].into(),
+            target_pos,
+            to_cast_against.clone(),
+        );
 
         /*
          *
@@ -123,6 +75,74 @@ async fn main() {
         );
 
         next_frame().await
+    }
+}
+
+fn shape_cast_debug(
+    screen_shift: Point2<f32>,
+    source_pos: Point2<f32>,
+    target_pos: Point2<f32>,
+    to_cast_against: ConvexPolygon,
+) {
+    /*
+     *
+     * Prepare a Raycast and compute its result against the shape.
+     *
+     */
+    let ray = Ray::new(source_pos, target_pos - source_pos);
+
+    let pos1: Point2<f32> = source_pos.into();
+    let vel1 = target_pos - source_pos;
+    let g1 = Ball::new(BALLCAST_WIDTH);
+    let pos2 = [0.0, 0.0];
+    let vel2 = [0.0, 0.0];
+    let g2 = to_cast_against.clone_dyn();
+
+    let toi = query::cast_shapes(
+        &pos1.into(),
+        &vel1,
+        &g1,
+        &pos2.into(),
+        &vel2.into(),
+        &*g2,
+        ShapeCastOptions::with_max_time_of_impact(1.0),
+    )
+    .unwrap();
+
+    /*
+     *
+     * Render the raycast's result.
+     *
+     */
+    drawcircle_at(pos1, BALLCAST_WIDTH, RENDER_SCALE, screen_shift, ORANGE);
+
+    if let Some(toi) = toi {
+        if toi.time_of_impact == 0f32 {
+            draw_point(ray.origin, RENDER_SCALE, screen_shift, YELLOW);
+        } else {
+            drawline_from_to(
+                ray.origin,
+                (ray.point_at(toi.time_of_impact).coords).into(),
+                RENDER_SCALE,
+                screen_shift,
+                GREEN,
+            );
+        }
+        drawcircle_at(
+            (ray.point_at(toi.time_of_impact).coords).into(),
+            BALLCAST_WIDTH,
+            RENDER_SCALE,
+            screen_shift,
+            GREEN,
+        );
+    } else {
+        drawline_from_to(
+            ray.origin,
+            ray.origin + ray.dir,
+            RENDER_SCALE,
+            screen_shift,
+            RED,
+        );
     }
 }
 
