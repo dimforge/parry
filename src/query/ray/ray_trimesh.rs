3b8f7b0cecf157e099c9d2a90bbd9ac365e0c64a
Sébastien Crozet <sebcrozet@dimforge.com>
2025-05-13 18:09:47 +0200

    feat: add trimesh raycasting that account for backface/frontface culling

--- src/query/ray/ray_trimesh.rs
+++ src/query/ray/ray_trimesh.rs
@@ -42,3 +42,144 @@ impl RayCast for TriMesh {
             })
     }
 }
+
+// NOTE: implement the ray-cast with culling on its own submodule to facilitate feature gating.
+#[cfg(feature = "dim3")]
+mod ray_cast_with_culling {
+    use crate::math::{Isometry, Real, Vector};
+    use crate::partitioning::{Qbvh};
+    use crate::query::{Ray, RayIntersection};
+    use crate::shape::{FeatureId, Shape, TriMesh, Triangle, TypedSimdCompositeShape};
+    use crate::query::details::NormalConstraints;
+    use crate::query::ray::{RayCompositeShapeToiAndNormalBestFirstVisitor};
+
+    /// Controls which side of a triangle a ray-cast is allowed to hit.
+    #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+    pub enum RayCullingMode {
+        /// Ray-casting won’t hit back faces.
+        BackFacesCulling,
+        /// Ray-casting won’t hit front faces.
+        FrontFacesCulling,
+    }
+
+    impl RayCullingMode {
+        fn check(self, tri_normal: &Vector<Real>, ray_dir: &Vector<Real>) -> bool {
+            match self {
+                RayCullingMode::BackFacesCulling => tri_normal.dot(ray_dir) < 0.0,
+                RayCullingMode::FrontFacesCulling => tri_normal.dot(ray_dir) > 0.0
+            }
+        }
+    }
+
+    /// A utility shape with a `TypedSimdCompositeShape` implementation that skips triangles that
+    /// are back-faces or front-faces relative to a given ray and culling mode.
+    struct TriMeshWithCulling<'a> {
+        trimesh: &'a TriMesh,
+        culling: RayCullingMode,
+        ray: &'a Ray,
+    }
+
+    impl<'a> TypedSimdCompositeShape for TriMeshWithCulling<'a> {
+        type PartShape = Triangle;
+        type PartNormalConstraints = ();
+        type PartId = u32;
+
+        #[inline(always)]
+        fn map_typed_part_at(
+            &self,
+            i: u32,
+            mut f: impl FnMut(
+                Option<&Isometry<Real>>,
+                &Self::PartShape,
+                Option<&Self::PartNormalConstraints>,
+            ),
+        ) {
+            let tri = self.trimesh.triangle(i);
+            let tri_normal = tri.scaled_normal();
+
+            if self.culling.check(&tri_normal, &self.ray.dir) {
+                f(None, &tri, None)
+            }
+        }
+
+        #[inline(always)]
+        fn map_untyped_part_at(
+            &self,
+            i: u32,
+            mut f: impl FnMut(Option<&Isometry<Real>>, &dyn Shape, Option<&dyn NormalConstraints>),
+        ) {
+            let tri = self.trimesh.triangle(i);
+            let tri_normal = tri.scaled_normal();
+
+            if self.culling.check(&tri_normal, &self.ray.dir) {
+                f(
+                    None,
+                    &tri,
+                    None,
+                )
+            }
+        }
+
+        fn typed_qbvh(&self) -> &Qbvh<u32> {
+            self.trimesh.qbvh()
+        }
+    }
+
+    impl TriMesh {
+        /// Casts a ray on this triangle mesh transformed by `m`.
+        ///
+        /// Hits happening later than `max_time_of_impact` are ignored. In other words, hits are
+        /// only searched along the segment `[ray.origin, ray.origin + ray.dir * max_time_of_impact`].
+        ///
+        /// Hits on back-faces or front-faces are ignored, depending on the given `culling` mode.
+        #[inline]
+        pub fn cast_ray_with_culling(
+            &self,
+            m: &Isometry<Real>,
+            ray: &Ray,
+            max_time_of_impact: Real,
+            culling: RayCullingMode,
+        ) -> Option<RayIntersection> {
+            let ls_ray = ray.inverse_transform_by(m);
+            self.cast_local_ray_with_culling(&ls_ray, max_time_of_impact, culling)
+                .map(|inter| inter.transform_by(m))
+        }
+
+        /// Casts a ray on this triangle mesh.
+        ///
+        /// This is the same as [`TriMesh::cast_ray_with_culling`] except that the ray is given
+        /// in the mesh’s local-space.
+        pub fn cast_local_ray_with_culling(
+            &self,
+            ray: &Ray,
+            max_time_of_impact: Real,
+            culling: RayCullingMode,
+        ) -> Option<RayIntersection> {
+            let mesh_with_culling = TriMeshWithCulling {
+                trimesh: self,
+                culling,
+                ray,
+            };
+
+            let mut visitor = RayCompositeShapeToiAndNormalBestFirstVisitor::new(
+                &mesh_with_culling,
+                ray,
+                max_time_of_impact,
+                false,
+            );
+
+            self.qbvh()
+                .traverse_best_first(&mut visitor)
+                .map(|(_, (best, mut res))| {
+                    // We hit a backface.
+                    // NOTE: we need this for `TriMesh::is_backface` to work properly.
+                    if res.feature == FeatureId::Face(1) {
+                        res.feature = FeatureId::Face(best + self.indices().len() as u32)
+                    } else {
+                        res.feature = FeatureId::Face(best);
+                    }
+                    res
+                })
+        }
+    }
+}
