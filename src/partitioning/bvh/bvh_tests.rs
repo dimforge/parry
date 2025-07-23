994c0ec21d9e2108c1331d551e20ef2143766a8a
Sébastien Crozet <sebcrozet@dimforge.com>
2025-07-23 15:21:38 +0200

    chore: add regression test for bvh add/remove

---
+++ src/partitioning/bvh/bvh_tests.rs
@@ -0,0 +1,91 @@
+use crate::bounding_volume::Aabb;
+use crate::math::{Real, Vector};
+use crate::partitioning::{Bvh, BvhBuildStrategy};
+
+fn make_test_aabb(i: usize) -> Aabb {
+    Aabb::from_half_extents(Vector::repeat(i as Real).into(), Vector::repeat(1.0))
+}
+
+#[test]
+fn bvh_build_and_removal() {
+    // Check various combination of building pattern and removal pattern.
+    // The tree validity is asserted at every step.
+    #[derive(Copy, Clone, Debug)]
+    enum BuildPattern {
+        Ploc,
+        Binned,
+        Insert,
+    }
+
+    #[derive(Copy, Clone, Debug)]
+    enum RemovalPattern {
+        InOrder,
+        RevOrder,
+        EvenOdd,
+    }
+
+    for build_pattern in [
+        BuildPattern::Ploc,
+        BuildPattern::Binned,
+        BuildPattern::Insert,
+    ] {
+        for removal_pattern in [
+            RemovalPattern::InOrder,
+            RemovalPattern::RevOrder,
+            RemovalPattern::EvenOdd,
+        ] {
+            for len in 1..=100 {
+                std::println!(
+                    "Testing build: {:?}, removal: {:?}, len: {}",
+                    build_pattern,
+                    removal_pattern,
+                    len
+                );
+                let leaves: std::vec::Vec<_> = (0..len).map(make_test_aabb).collect();
+
+                let mut bvh = match build_pattern {
+                    BuildPattern::Binned => Bvh::from_leaves(BvhBuildStrategy::Binned, &leaves),
+                    BuildPattern::Ploc => Bvh::from_leaves(BvhBuildStrategy::Ploc, &leaves),
+                    BuildPattern::Insert => {
+                        let mut bvh = Bvh::new();
+                        for i in 0..len {
+                            bvh.insert(make_test_aabb(i), i as u32);
+                            bvh.assert_well_formed();
+                        }
+                        bvh
+                    }
+                };
+
+                bvh.assert_well_formed();
+
+                match removal_pattern {
+                    RemovalPattern::InOrder => {
+                        // Remove in insertion order.
+                        for i in 0..len {
+                            bvh.remove(i as u32);
+                            bvh.assert_well_formed();
+                        }
+                    }
+                    RemovalPattern::RevOrder => {
+                        // Remove in reverse insertion order.
+                        for i in (0..len).rev() {
+                            bvh.remove(i as u32);
+                            bvh.assert_well_formed();
+                        }
+                    }
+                    RemovalPattern::EvenOdd => {
+                        // Remove even indices first, then odd.
+                        for i in (0..len).filter(|i| i % 2 == 0) {
+                            bvh.remove(i as u32);
+                            bvh.assert_well_formed();
+                        }
+                        for i in (0..len).filter(|i| i % 2 != 0) {
+                            bvh.remove(i as u32);
+                            bvh.assert_well_formed();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
