a135ccef38432b5cac486bb3c3f49a17e9e12785
Sébastien Crozet <sebcrozet@dimforge.com>
2025-08-08 17:10:42 +0200

    fix benches compilation

--- crates/parry3d/benches/common/default_gen.rs
+++ crates/parry3d/benches/common/default_gen.rs
@@ -7,7 +7,7 @@ use parry3d::math::{Point, Real, Vector};
 use parry3d::query::Ray;
 use parry3d::shape::ConvexPolyhedron;
 use parry3d::shape::{Ball, Capsule, Cone, Cuboid, Cylinder, Segment, Triangle};
-use rand::distributions::{Distribution, Standard};
+use rand::distr::{Distribution, StandardUniform};
 use rand::Rng;
 
 pub trait DefaultGen {
@@ -22,7 +22,7 @@ macro_rules! impl_rand_default_gen (
     ($t: ty) => {
         impl DefaultGen for $t {
             fn generate<R: Rng>(rng: &mut R) -> $t {
-                rng.gen::<$t>()
+                rng.random::<$t>()
             }
         }
     }
@@ -56,101 +56,101 @@ impl_rand_default_gen!(bool);
 
 impl DefaultGen for Ball
 where
-    Standard: Distribution<Real>,
+    StandardUniform: Distribution<Real>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Ball {
-        Ball::new(rng.gen::<f32>().abs())
+        Ball::new(rng.random::<f32>().abs())
     }
 }
 
 impl DefaultGen for Cuboid
 where
-    Standard: Distribution<Vector<Real>>,
+    StandardUniform: Distribution<Vector<Real>>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Cuboid {
-        Cuboid::new(rng.gen::<Vector<Real>>().abs())
+        Cuboid::new(rng.random::<Vector<Real>>().abs())
     }
 }
 
 impl DefaultGen for Capsule
 where
-    Standard: Distribution<Real>,
+    StandardUniform: Distribution<Real>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Capsule {
         Capsule::new(
-            rng.gen::<Point<Real>>(),
-            rng.gen::<Point<Real>>(),
-            rng.gen::<Real>().abs(),
+            rng.random::<Point<Real>>(),
+            rng.random::<Point<Real>>(),
+            rng.random::<Real>().abs(),
         )
     }
 }
 
 impl DefaultGen for Cone
 where
-    Standard: Distribution<Real>,
+    StandardUniform: Distribution<Real>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Cone {
-        Cone::new(rng.gen::<Real>().abs(), rng.gen::<Real>().abs())
+        Cone::new(rng.random::<Real>().abs(), rng.random::<Real>().abs())
     }
 }
 
 impl DefaultGen for Cylinder
 where
-    Standard: Distribution<Real>,
+    StandardUniform: Distribution<Real>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Cylinder {
-        Cylinder::new(rng.gen::<Real>().abs(), rng.gen::<Real>().abs())
+        Cylinder::new(rng.random::<Real>().abs(), rng.random::<Real>().abs())
     }
 }
 
 impl DefaultGen for Segment
 where
-    Standard: Distribution<Point<Real>>,
+    StandardUniform: Distribution<Point<Real>>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Segment {
-        Segment::new(rng.gen(), rng.gen())
+        Segment::new(rng.random(), rng.random())
     }
 }
 
 impl DefaultGen for Triangle
 where
-    Standard: Distribution<Point<Real>>,
+    StandardUniform: Distribution<Point<Real>>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Triangle {
-        Triangle::new(rng.gen(), rng.gen(), rng.gen())
+        Triangle::new(rng.random(), rng.random(), rng.random())
     }
 }
 
 impl DefaultGen for ConvexPolyhedron
 where
-    Standard: Distribution<Real>,
+    StandardUniform: Distribution<Real>,
 {
     fn generate<R: Rng>(rng: &mut R) -> ConvexPolyhedron {
         // It is recommended to have at most 100 points.
         // Otherwise, a smarter structure like the DK hierarchy would be needed.
-        let pts: Vec<_> = (0..100).map(|_| rng.gen()).collect();
+        let pts: Vec<_> = (0..100).map(|_| rng.random()).collect();
         ConvexPolyhedron::from_convex_hull(&pts).unwrap()
     }
 }
 
 impl DefaultGen for Ray
 where
-    Standard: Distribution<Vector<Real>>,
+    StandardUniform: Distribution<Vector<Real>>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Ray {
         // The generate ray will always point to the origin.
-        let shift = rng.gen::<Vector<Real>>() * na::convert::<_, Real>(10.0f64);
+        let shift = rng.random::<Vector<Real>>() * na::convert::<_, Real>(10.0f64);
         Ray::new(Point::origin() + shift, -shift)
     }
 }
 
 impl DefaultGen for Aabb
 where
-    Standard: Distribution<Vector<Real>>,
+    StandardUniform: Distribution<Vector<Real>>,
 {
     fn generate<R: Rng>(rng: &mut R) -> Aabb {
         // an Aabb centered at the origin.
-        let half_extents = rng.gen::<Vector<Real>>().abs();
+        let half_extents = rng.random::<Vector<Real>>().abs();
         Aabb::new(
             Point::origin() + (-half_extents),
             Point::origin() + half_extents,
@@ -160,10 +160,10 @@ where
 
 impl DefaultGen for BoundingSphere
 where
-    Standard: Distribution<Real>,
+    StandardUniform: Distribution<Real>,
 {
     fn generate<R: Rng>(rng: &mut R) -> BoundingSphere {
         // a bounding sphere centered at the origin.
-        BoundingSphere::new(Point::origin(), rng.gen::<Real>().abs())
+        BoundingSphere::new(Point::origin(), rng.random::<Real>().abs())
     }
 }
--- crates/parry3d/benches/common/generators.rs
+++ crates/parry3d/benches/common/generators.rs
@@ -3,7 +3,9 @@ use parry3d::shape::TriMesh;
 use rand::Rng;
 
 pub fn generate_trimesh_around_origin<R: Rng>(rng: &mut R) -> TriMesh {
-    let pts = (0..3000).map(|_| rng.gen::<Point3<f32>>() * 3.0).collect();
+    let pts = (0..3000)
+        .map(|_| rng.random::<Point3<f32>>() * 3.0)
+        .collect();
     let indices = (0..1000).map(|i| [i * 3, i * 3 + 1, i * 3 + 2]).collect();
 
     TriMesh::new(pts, indices).unwrap()
