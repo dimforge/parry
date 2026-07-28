9d33bc4e8755cc72a87e6c7c0a73e8b36efbc200
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-07-27 19:50:48 -0700

    Fix typo

--- src/shape/cuboid.rs
+++ src/shape/cuboid.rs
@@ -309,7 +309,7 @@ impl Cuboid {
         // The vertex id as numbered depending on the sign of the vertex
         // component. A + sign means the corresponding bit is 0 while a -
         // sign means the corresponding bit is 1.
-        // For exampl the vertex [2.0, -1.0, -3.0] has the id 0b011
+        // For example the vertex [2.0, -1.0, -3.0] has the id 0b011
         let vids = match imax {
             0 => [
                 [vid(0b000), vid(0b010), vid(0b011), vid(0b001)],
